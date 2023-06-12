cce2488e8e5fca1632a7a839550b7ffd73202fa6
LuizHenriquemf <luizhenriquefurquim@hotmail.com>
2023-06-12 20:12:51 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='headerContainer'>
+            <header>Header</header>
+        </div>
+    );
+}
+
+export default Header;
